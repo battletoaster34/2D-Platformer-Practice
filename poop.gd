e5c2d4e9f2dc26db05d4d52f8b1bc722981086da
Madbloop <madblooperminecraft@gmail.com>
2023-11-17 10:50:52 -0500

    Sprinting

--- poop.gd
+++ poop.gd
@@ -1,100 +1,80 @@
 extends CharacterBody2D
 
-
-var SPEED = 150.0
-const ACCELERATION = 800.0
-const FRICTION = 800.0
 var insidesomething = false
+var SPEED = 150.0
 var crouching = false
 var sprinting = false
 const JUMP_VELOCITY = -350.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-@onready var coyote_jump_timer = $CoyoteJumpTimer # gives acces to timer
-
 
 func _physics_process(delta):
 	# Add the gravity.
 
-	_apply_gravity(delta)
-
+	if Input.is_action_just_pressed("Reset"):
+		position.x = 0
+		position.y = 0
 	
-	_handle_jump()
-
-
-	var input_axis = Input.get_axis("left", "right")
-		
-	_handle_acceleration(input_axis, delta)
-		
-	_apply_friction(input_axis, delta)
-
-	_handle_crouch()
-	_handle_sprint()
-	_handle_animation()
-	
-	_reset()
-
-	move_and_slide()
-		
-			
-		
-func _apply_gravity(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 		$AnimatedSprite2D.animation = "Jump"
 		$AnimatedSprite2D.play()
-	# Get the input direction and handle the movement/deceleration.
-func _handle_jump():
-	if is_on_floor():
-		if insidesomething == false:
-			if Input.is_action_just_pressed("Jump"):
-				velocity.y = JUMP_VELOCITY
-				#makes a short jump if the jump button is pressed for a short time
 
-		else:
-			if Input.is_action_just_pressed("Jump"):
-				velocity.y = JUMP_VELOCITY
-	#makes a short jump if the jump button is pressed for a short time
-
-func _handle_crouch():
-	if sprinting == false:
-		if Input.is_action_pressed("crouch") and is_on_floor():
-			SPEED = 50
-			if crouching == false:
-				$AnimatedSprite2D.animation = "crouchidlestart"
-				$AnimatedSprite2D.play()	
+	# Handle Jump.
+	if insidesomething == false:
+		if Input.is_action_just_pressed("Jump") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
+		
+		
+	#CROUCHING	
+	if Input.is_action_pressed("crouch") and is_on_floor():
+		SPEED = 50
+		if crouching == false:
+			$AnimatedSprite2D.animation = "crouchidlestart"
+			$AnimatedSprite2D.play()	
+			
+			abovesensorlist = $Area2D2.get_overlapping_bodies()
+			removeselffromarray(abovesensorlist)
+			if self not in abovesensorlist and len(abovesensorlist) != 0:
+				insidesomething = true
 				
-				abovesensorlist = $Area2D2.get_overlapping_bodies()
-				removeselffromarray(abovesensorlist)
-				if self not in abovesensorlist and len(abovesensorlist) != 0:
-					insidesomething = true
-			
-			crouching = true
-			$Crouchinghitbox.disabled = false
-			$Standinghitbox.disabled = true
-			
-		else:
-			if insidesomething == false:
-				crouching = false
-				SPEED = 150	
-				$Crouchinghitbox.disabled = true
-				$Standinghitbox.disabled = false
-func _handle_sprint():
-	if crouching == false:
-		if Input.is_action_pressed("Sprint"):
-			SPEED = 200
-			if sprinting == false:
-				$AnimatedSprite2D.animation = "Sprint"
+			if velocity.x != 0:
+				$AnimatedSprite2D.animation = "crouchsidestart"
 				$AnimatedSprite2D.flip_v = false
 				$AnimatedSprite2D.flip_h = velocity.x < 0
 				$AnimatedSprite2D.play()
-			sprinting = true
-		else:
-			sprinting = false
-			if crouching == false:
-				SPEED = 150
-func _handle_animation():
+
+		crouching = true
+		
+		$Crouchinghitbox.disabled = false
+		$Standinghitbox.disabled = true
+		
+	else:
+		if insidesomething == false:
+			crouching = false
+			SPEED = 150	
+			$Crouchinghitbox.disabled = true
+			$Standinghitbox.disabled = false
+		
+	if Input.is_action_pressed("Sprint"):
+		SPEED = 200
+		if sprinting == false:
+			$AnimatedSprite2D.animation = "Sprint"
+			$AnimatedSprite2D.flip_v = false
+			$AnimatedSprite2D.flip_h = velocity.x < 0
+			$AnimatedSprite2D.play()
+		sprinting = true
+	else:
+		sprinting = false
+		if crouching == false:
+			SPEED = 150
+
+			
+		
+		
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
 	if is_on_floor():
 		if velocity.x != 0:
 			if crouching == false and sprinting == false:
@@ -123,7 +103,16 @@ func _handle_animation():
 			if ($AnimatedSprite2D.animation == "crouchidlestart" and $AnimatedSprite2D.is_playing() == false) or $AnimatedSprite2D.animation != "crouchidlestart":
 				$AnimatedSprite2D.animation = "crouchidle"
 				$AnimatedSprite2D.play()	
+	var direction = Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
 	
+
+	move_and_slide()
+
+
 
 func removeselffromarray(arrayc):
 		if len(arrayc) > 1:
@@ -133,12 +122,7 @@ func removeselffromarray(arrayc):
 		else:
 			if self in arrayc:
 				arrayc.erase(arrayc[0])
-				
-func _reset():
-	if Input.is_action_pressed("Reset"):
-		position.x=0
-		position.y=0
-		
+
 
 var abovesensorlist
 var abovesensorlist2
@@ -152,18 +136,8 @@ func _on_area_2d_2_body_entered(body):
 
 
 func _on_area_2d_2_body_exited(body):
-	abovesensorlist = $Area2D2.get_overlapping_bodies()
-	removeselffromarray(abovesensorlist)
-	if self not in abovesensorlist2 and abovesensorlist2 != abovesensorlist:
-		insidesomething = false
-func _apply_friction(input_axis, delta):
-	if input_axis == 0:
-		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
 	
 	abovesensorlist2 = $Area2D2.get_overlapping_bodies()
 	removeselffromarray(abovesensorlist2)
 	if self not in abovesensorlist2 and abovesensorlist2 != abovesensorlist:
 		insidesomething = false
-func _handle_acceleration(input_axis, delta):
-	if input_axis != 0:
-		velocity.x = move_toward(velocity.x, SPEED * input_axis, ACCELERATION * delta)
